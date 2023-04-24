f97aacc27b16b8860c16100a37aed13f40eeac6a
Itzell <mmitzel2435@gmail.com>
2023-04-23 21:31:26 -0600

    create v1

---
+++ src/main/java/org/uv/programa03/Programa03.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package org.uv.programa03;
+
+/**
+ *
+ * @author Itzel
+ */
+public class Programa03 {
+
+    public static void main(String[] args) {
+        
+        ClaseConcreta c = new ClaseConcreta();
+        System.out.println("Hello World!");
+    }
+}
